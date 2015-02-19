0d701e5d762e9a2d23788210fae2913d2299c4c3
Alexandre Devienne <grypoB@gmail.com>
2015-02-19 21:05:47 +0100

    Add work to do in geometrie.h
    some basic function i need for particule.c

--- source/geometrie.h
+++ source/geometrie.h
@@ -6,4 +6,17 @@ typedef struct {
     double y;
 } POINT;
 
+typedef struct {
+    double x;
+    double y;
+} VECTOR;
+
+#if 1
+// function to do (2015-02-19)
+POINT point_null(); // return a point of coord (0,0)
+VECTOR vector_null(); // return the null vector (0,0)
+
+VECTOR vector_sum(VECTOR v1, VECTOR v2);
+#endif
+
 #endif
