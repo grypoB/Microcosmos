bd2609c3436cd64fdb7d7efe03cac2d825c8ec3a
Alexandre Devienne <grypoB@gmail.com>
2015-02-19 21:06:35 +0100

    Add constance.h tolerance.h
    basic constance given by the professor needed for the project

---
+++ source/constante.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTE_H
+#define CONSTANTE_H
+#include "tolerance.h"
+
+#define MAX_RENDU1 10 
+#define KMASSE 0.1 
+#define RMIN 0.5 
+#define RMAX 50 
+#define RBLACK 100 
+#define FBLACK 9 
+#define FBLACK_MIN 1 
+#define MAX_ATTR 20 
+#define MAX_REP -100 
+#define DELTA_T 0.1 
+#define MAX_VITESSE 10 
+#define NBP 0.05
+
+#endif
---
+++ source/tolerance.h
@@ -0,0 +1,6 @@
+#ifndef TOLERANCE_H
+#define TOLERANCE_H
+
+#define EPSILON_ZERO 1e-9 
+
+#endif
