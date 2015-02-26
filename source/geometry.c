edb3673b216f00c8fd900b02766b2ddf18b71ffe
Pauline Maury Laribiere <paulineml@hotmail.com>
2015-02-26 16:12:46 +0100

    update geometrie.c

--- source/geometry.c
+++ source/geometry.c
@@ -14,3 +14,72 @@ double linear_interpolation(double xC,
         return 0;
     }
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct   //COORDONNEE D'UN POINT
+{
+	double x;
+	double y;
+}COORDONNEE;
+
+typedef struct   //COORDONNEE D'UN VECTEUR
+{
+	double x;
+	double y;
+}VECTEUR;
+
+typedef struct  // PARTICULE
+{
+	double rayon;
+	COORDONNEE centre;
+}PARTICULE;
+
+int info_particule(PARTICULE particule_1)
+{
+	//printf("Entrer le rayon de la particule:\n");
+	scanf("%lf", &(particule_1.rayon)); 
+	
+	//printf("Entrer la coordonnée x du centre de la particule:\n");
+	scanf("%lf", &(particule_1.centre.x)); 
+	
+	//printf("Entrer la coordonnée y du centre de la particule:\n");
+	scanf("%lf", &(particule_1.centre.y)); 
+	
+	return 0;
+}
+
+double distances_particules(PARTICULE particule_1, PARTICULE particule_2)
+{
+	double distance_abscisse = particule_1.centre.x - particule_2.centre.x;
+	double distance_ordonnee = particule_1.centre.y - particule_2.centre.y;
+	double distance_entre_particules = sqrt(distance_abscisse * distance_abscisse + distance_ordonnee  * distance_ordonnee);
+	
+	return distance_entre_particules;
+}
+
+VECTEUR addition_vecteurs(VECTEUR vecteur_1, VECTEUR vecteur_2)
+{
+	VECTEUR vecteur_resultant;
+	vecteur_resultant.x= vecteur_1.x + vecteur_2.x;
+	vecteur_resultant.y= vecteur_1.y + vecteur_2.y;
+	return vecteur_resultant;
+}
+
+double norme_vecteur(VECTEUR vecteur_1)
+{
+	double norme_du_vecteur;
+	norme_du_vecteur =sqrt(vecteur_1.x * vecteur_1.x + vecteur_1.y  * vecteur_1.y);
+	return norme_du_vecteur;
+}
+
+VECTEUR multiplication_vecteurs(VECTEUR vecteur, double a)
+{
+	vecteur.x = vecteur.x * a;
+	vecteur.y = vecteur.y * a;
+	return vecteur;
+}
+
+
