664cb94454f1cb565819182207ea5e75718086e3
Alexandre Devienne <grypoB@gmail.com>
2015-03-04 16:09:52 +0100

    Resolve merge
    in geomertry.h

--- source/geometry.h
+++ source/geometry.h
@@ -11,40 +11,6 @@ typedef struct {
     double y;
 } VECTOR;
 
-<<<<<<< HEAD
-// // //
-
-typedef struct  // PARTICULE
-{
-	double nb_particules;
-	double rayon;
-	double posx;
-	double posy;
-	double vx;
-	double vy;
-}PARTICULE;
-
-typedef struct //GENERATEUR
-{
-	double nb_de_generateurs;
-	double rgen;
-	double posx;
-	double posy;
-	double vpi_x;
-	double vpi_y;
-}GENERATEUR;
-
-typedef struct //TROU NOIR
-{
-	double nb_de_trous_noirs;
-	double posx;
-	double posy;
-}TROU_NOIR;
-
-#if 1
-// function to do (2015-02-19)
-=======
->>>>>>> 9300a8756d018df58fffbb8d37b646df49198ec9
 POINT point_null(); // return a point of coord (0,0)
 POINT point_create(double x, double y);
 double point_distance(POINT p1, POINT p2);
