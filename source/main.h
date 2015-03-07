ad5a2286826e5c2b18a4e0c2420e1a7ba3c0fa51
Pauline Maury Laribiere <paulineml@hotmail.com>
2015-03-07 21:40:46 +0100

    créer nouveau main.h

---
+++ source/main.h
@@ -0,0 +1,14 @@
+/* Nom: main.h
+ * Description: 
+ * Date: 04.02.2014
+ * version : 1.1
+ * auteur : PROG II
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int main (int argc, char *argv[]);
+void read_mode(int* mode, char *argv[]);
+
+#endif
