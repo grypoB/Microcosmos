a70f0a32cbd2c0cafdf79c79400f26b827d81f03
Pauline Maury Laribiere <paulineml@hotmail.com>
2015-03-07 17:55:29 +0100

    update main.cpp apres discussion fb

--- source/main.cpp
+++ source/main.cpp
@@ -1,24 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "error.h"
 #include "sim.h"
-#include "particule.h"
+
+enum Mode {ERROR, FORCE, INTEGRATION, GRAPHIC, SIMULATION};
 
 int main (int argc, char *argv[])
-{
-	enum argument {Error, Force, Integration, Graphic, Simulation};
-	switch(*argv[1]) 
+{ 
+    enum Mode mode;
+    
+    if(strcmp(argv[1], "Error" ) == 0)
+    { 
+		mode = ERROR;
+    }
+    else if (strcmp(argv[1], "Force" ) == 0)
+    { 
+		mode = FORCE;
+    }
+    else if (strcmp(argv[1], "Integration" ) == 0)
+    { 
+		mode = INTEGRATION;
+    }
+    else if (strcmp(argv[1], "Graphic" ) == 0)
+    { 
+		mode = GRAPHIC;
+    }
+    else if (strcmp(argv[1], "Simulation" ) == 0)
+    { 
+		mode = SIMULATION;
+    }
+    
+	if(argc==1)
 	{
-		case Error: sim_error(argv[2]);
-		case Force: sim_force(argv[2]);
-		case Integration: sim_integration(argv[2]);
-		case Graphic: sim_graphic(argv[2]);
-		case Simulation: sim_simulation(argv[2]);
-		default: error_msg("invalid argument in main (main.cpp)");
+		//initialise interface graphique //demo.x a revoir
 	}
-	return 0;
+	else
+	{
+		switch(mode) 
+		{
+			case ERROR: sim_error(argv[2]);
+			break;
+			case FORCE: sim_force(argv[2]);
+			break;
+			case INTEGRATION: sim_integration(argv[2]);
+			break;
+			case GRAPHIC: sim_graphic(argv[2]);
+			break;
+			case SIMULATION: sim_simulation(argv[2]);
+			break;
+			//default: error_msg("invalid argument in main (main.cpp)");
+		}
+	}
+	return EXIT_SUCCESS;
 }
 
-//argv[0] pointe sur une chaîne vide.
-//argv[1] mode de test
-//argv[2] nom du fichier
