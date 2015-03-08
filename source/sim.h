ed480ef19ba1a2fdad277757946bffac4418c228
Pauline Maury Laribiere <paulineml@hotmail.com>
2015-03-08 08:48:46 +0100

    uptdate sim.h pourfonctionnement de main.cpp

--- source/sim.h
+++ source/sim.h
@@ -1,16 +1,18 @@
 #ifndef SIM_H
 #define SIM_H
 
+#include <stdbool.h>
+
 bool sim_lecture(char filename[]);
 
 void sim_error(char filename[]);
 void sim_force(char filename[]);
 
-/*
+
 void sim_integration(char filename[]);
 void sim_graphic(char filename[]);
 void sim_simulation(char filename[]);
-*/
+
 
 void sim_clean();
 
