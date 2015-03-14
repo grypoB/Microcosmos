f9d5ba1c6a161dddd248cca9810367e4d621a5ab
Pauline Maury Laribiere <paulineml@hotmail.com>
2015-03-14 21:43:03 +0100

    main.cpp souris clavier idle et callback fonction pret

--- source/main.cpp
+++ source/main.cpp
@@ -13,6 +13,10 @@
 #include <GL/glu.h>
 #include <GL/glui.h>
 
+#define NB_PARTICULE 1
+#define NB_GENERATEUR 2
+#define NB_TROU_NOIR 3
+
 extern "C"
 {
 #include "constantes.h"
@@ -26,6 +30,7 @@ namespace
 	GLUI_Panel *file;
 	GLUI_Panel *simulation;
 	GLUI_Panel *information;
+	char* live_var;
 }
 
 //fonction pour opengl
@@ -33,6 +38,7 @@ void affichage(void);
 void reshape(int w, int h);
 void keyboard(unsigned char Key, int x, int y);
 void mouse(int button, int button_state, int x, int y );
+void idle(void);
 
 
 //fonction pour le mode GRAPHIC
@@ -122,11 +128,11 @@ void initOpenGl(void)
 	
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // ??
 	glutInitWindowPosition(200, 200);
-	glutInitWindowSize(250, 250);  //taille de la fenetre (pas dans la donnée..)
+	glutInitWindowSize(250, 250);
 	
-	main_window = glutCreateWindow("Microcosmos");  //crée la fenetre
+	main_window = glutCreateWindow("Microcosmos");
 
-	glClearColor(1.0, 1.0, 1.0, 0.0);  //pour le fond blanc 
+	glClearColor(1.0, 1.0, 1.0, 0.0);
 	
 	glutDisplayFunc(affichage);
 	glutReshapeFunc(reshape);
@@ -134,74 +140,82 @@ void initOpenGl(void)
 	glutMouseFunc(mouse);
 	
 	/*
-	 * AJOUTER OPENGL ET GRAPHIQUE ICI
+	 * AJOUTER OPENGL appeler graphic.c ici
+	 * revoir aussi affichage
 	 */
 		
 	/*Code GLUI pour l'interface*/
 	GLUI *glui = GLUI_Master.create_glui( "GLUI", 0, 400, 50 );
 	
-	//File avec filename load et save
 	file = glui->add_panel("File" );
-	glui-> add_edittext_to_panel(file, "Filename", GLUI_EDITTEXT_TEXT, NULL, -1, load_cb);  // recoit un nom pour openfile
-    glui-> add_button_to_panel(file,"Load", -1, load_cb);                       //ouvre le fichier dont le nom est donné
+	glui-> add_edittext_to_panel(file, "Filename", GLUI_EDITTEXT_TEXT, live_var, -1, load_cb);
+    glui-> add_button_to_panel(file,"Load", -1, load_cb);
     
-    glui-> add_edittext_to_panel(file, "Filename", GLUI_EDITTEXT_TEXT, NULL, -1, save_cb);  //recoit un nom pour enregistrer
-    glui-> add_button_to_panel(file,"Save", -1, save_cb);                    //enregistre etat actuelde simulation dans ce fichier
+    glui-> add_edittext_to_panel(file, "Filename", GLUI_EDITTEXT_TEXT, live_var, -1, save_cb);
+    glui-> add_button_to_panel(file,"Save", -1, save_cb);
     
 	
-	//Simulation avec start et step
 	simulation = glui->add_panel("Simulation" );
-	glui->add_button_to_panel( simulation ,"Start", 1, simulation_cb);  //bouton pour commencer/stopper la simulation
-	glui->add_button_to_panel( simulation ,"Step", 2, simulation_cb);   //lorsque la simulation est stoppée -> calcule seulement un pas
-
+	glui->add_button_to_panel( simulation ,"Start", 1, simulation_cb);
+	glui->add_button_to_panel( simulation ,"Step", 2, simulation_cb);
 	
-	//Information avec nb elements
-	information = glui->add_panel("Information" );   //affiche les infos
-	glui-> add_edittext_to_panel(information, "Nb Particule", GLUI_EDITTEXT_INT, NULL, -1, information_cb); 
-	glui-> add_edittext_to_panel(information, "Nb Generateur", GLUI_EDITTEXT_INT, NULL, -1, information_cb);
-	glui-> add_edittext_to_panel(information, "Nb Trou noir", GLUI_EDITTEXT_INT, NULL, -1, information_cb);
+	information = glui->add_panel("Information" );
+	glui-> add_edittext_to_panel(information, "Nb Particule", GLUI_EDITTEXT_INT, NULL, NB_PARTICULE, information_cb); 
+	glui-> add_edittext_to_panel(information, "Nb Generateur", GLUI_EDITTEXT_INT, NULL, NB_GENERATEUR, information_cb);
+	glui-> add_edittext_to_panel(information, "Nb Trou noir", GLUI_EDITTEXT_INT, NULL, NB_TROU_NOIR, information_cb);
 	
-	glui->add_button( "Quit",0, quit_cb );  //doit encore fermer les fenetres et fichiers puis quitter programme
+	glui->add_button( "Quit",0, quit_cb );
 	
-	/*quand un widget de GLUI change, un évènement de redisplay est envoyé à la main_gfx_window */
 	glui->set_main_gfx_window( main_window );
 	
-	/**** Regular GLUT main loop ****/
+	GLUI_Master.set_glutIdleFunc(idle);
+
 	glutMainLoop();
 
 }
 
-void load_cb(int control)
+void load_cb(int control, char live_var)
 {
-	//edittext->get_text();
+	// recoit un nom pour openfile
+	//ouvre le fichier dont le nom est donné
 }
 
 void save_cb(int control)
 {
-	
+	//recoit un nom pour enregistrer
+	//enregistre etat actuel de simulation dans ce fichier
 }
 
 void simulation_cb(int control)
 {
-	
+	//start:bouton pour commencer/stopper la simulation
+	//step:lorsque la simulation est stoppée -> calcule seulement un pas // enable seulement quand simulation est stoppée
 }
 void information_cb(int control)
 {
-	
+	switch(control)
+	{
+		case NB_PARTICULE: //mettre a jour
+		break;
+		case NB_GENERATEUR: //mettre a jour
+		break;
+		case NB_TROU_NOIR: //mettre a jour
+		break;
+		
+		return;
+	}
 }
 
 void quit_cb(int control)
 {
-	
+	//fermer les fenetres et fichiers puis quitter programme
 }
 
 void affichage(void)
 {
-	// variables qui définissent le domaine visualisé avec Ortho
 	GLfloat gauche= -RMAX, droite = RMAX, bas= -RMAX, haut= RMAX;
 	glClear(GL_COLOR_BUFFER_BIT);
 	
-	// ré-initialisation de la matrice de transformation
 	glLoadIdentity();
 	
 	if (aspect_ratio <= 1.)
@@ -210,20 +224,14 @@ void affichage(void)
 	else
 	glOrtho(gauche*aspect_ratio, droite*aspect_ratio, bas, haut, -1.0, 1.0);
 	
-	//fct qui créent particules
 	glutSwapBuffers();
 }
 
 void reshape(int w, int h)
 {
-	//le viewport prend toute la fenêtre
 	glViewport(0, 0, w, h);
-	
-	// mise à jour pour usage futur par affichage()
 	aspect_ratio = (GLfloat) w / (GLfloat) h ;
-	
-	// création d'un event demandant un ré-affichage
-	glutPostRedisplay();
+	glutPostRedisplay(); 
 }
 
 void mouse(int button, int button_state, int x, int y )
@@ -236,18 +244,26 @@ void mouse(int button, int button_state, int x, int y )
   }
 }
 
-void keyboard(unsigned char Key, int x, int y) //si une entité est selectionnée, elle est détruite quand on appuit sur d
+void keyboard(unsigned char Key, int x, int y)
 {
 	if(1)  // si une entitée est selectionnée
 	{	switch(Key)
 		{
-			case 'd':
+			case 'd': //détruire entitée
 			break;
 			default :
 			break;
 		}
 	}
   
+  glutPostRedisplay();
+}
+
+void idle(void)
+{
+	if ( glutGetWindow() != main_window ) 
+		glutSetWindow(main_window);  
+
   glutPostRedisplay();
 }
 
