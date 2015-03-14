32810b706302ba78ae3458a94f51ae45d28aa947
Pauline Maury Laribiere <paulineml@hotmail.com>
2015-03-14 20:32:02 +0100

    main.cpp avec début pour souris et clavier

--- source/main.cpp
+++ source/main.cpp
@@ -31,13 +31,19 @@ namespace
 //fonction pour opengl
 void affichage(void);
 void reshape(int w, int h);
+void keyboard(unsigned char Key, int x, int y);
+void mouse(int button, int button_state, int x, int y );
+
 
 //fonction pour le mode GRAPHIC
 void initOpenGl(void);
 
 //fonction callback (interface)
-void file_cb(int control);
-void control_cb(int control);
+void load_cb(int control);
+void save_cb(int control);
+void simulation_cb(int control);
+void information_cb(int control);
+void quit_cb(int control);
 
 // Mode of the simulation to be ran on, see specs sheet for details
 enum Mode {ERROR, FORCE, INTEGRATION, GRAPHIC, SIMULATION, MODE_UNSET};
@@ -66,6 +72,7 @@ int main (int argc, char *argv[])
 		case INTEGRATION: //sim_integration(argv[2]);
 		break;
 		case GRAPHIC: //sim_graphic(argv[2]);
+			 glutInit(&argc, argv);
 			 initOpenGl();
 		break;
 		case SIMULATION: //sim_simulation(argv[2]);
@@ -111,41 +118,50 @@ static MODE read_mode(const char string[])
 
 void initOpenGl(void)
 {
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutInitWindowPosition(200, 200);
-	glutInitWindowSize(250, 250);
+	/*Initialise Glut and Create Window*/
 	
-	main_window = glutCreateWindow("Microcosmos");
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // ??
+	glutInitWindowPosition(200, 200);
+	glutInitWindowSize(250, 250);  //taille de la fenetre (pas dans la donnée..)
+	
+	main_window = glutCreateWindow("Microcosmos");  //crée la fenetre
 
-	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClearColor(1.0, 1.0, 1.0, 0.0);  //pour le fond blanc 
 	
 	glutDisplayFunc(affichage);
 	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutMouseFunc(mouse);
 	
+	/*
+	 * AJOUTER OPENGL ET GRAPHIQUE ICI
+	 */
+		
+	/*Code GLUI pour l'interface*/
 	GLUI *glui = GLUI_Master.create_glui( "GLUI", 0, 400, 50 );
 	
 	//File avec filename load et save
 	file = glui->add_panel("File" );
-	glui-> add_edittext_to_panel(file, "Filename", GLUI_EDITTEXT_TEXT, NULL, -1, file_cb);
-    glui-> add_button_to_panel(file,"Load", -1, file_cb);
+	glui-> add_edittext_to_panel(file, "Filename", GLUI_EDITTEXT_TEXT, NULL, -1, load_cb);  // recoit un nom pour openfile
+    glui-> add_button_to_panel(file,"Load", -1, load_cb);                       //ouvre le fichier dont le nom est donné
     
-    glui-> add_edittext_to_panel(file, "Filename", GLUI_EDITTEXT_TEXT, NULL, -1, file_cb);
-    glui-> add_button_to_panel(file,"Save", -1, file_cb);
+    glui-> add_edittext_to_panel(file, "Filename", GLUI_EDITTEXT_TEXT, NULL, -1, save_cb);  //recoit un nom pour enregistrer
+    glui-> add_button_to_panel(file,"Save", -1, save_cb);                    //enregistre etat actuelde simulation dans ce fichier
     
 	
 	//Simulation avec start et step
 	simulation = glui->add_panel("Simulation" );
-	glui->add_button_to_panel( simulation ,"Start", -1, GLUI_CB());  //bouton pour commencer/stopper la simulation
-	glui->add_button_to_panel( simulation ,"Step", -1, GLUI_CB());   //lorsque la simulation est stoppée -> calcule seulement un pas
+	glui->add_button_to_panel( simulation ,"Start", 1, simulation_cb);  //bouton pour commencer/stopper la simulation
+	glui->add_button_to_panel( simulation ,"Step", 2, simulation_cb);   //lorsque la simulation est stoppée -> calcule seulement un pas
 
 	
 	//Information avec nb elements
-	information = glui->add_panel("Information" );
-	glui-> add_edittext_to_panel(information, "Nb Particule", GLUI_EDITTEXT_INT, NULL, -1, control_cb);
-	glui-> add_edittext_to_panel(information, "Nb Generateur", GLUI_EDITTEXT_INT, NULL, -1, control_cb);
-	glui-> add_edittext_to_panel(information, "Nb Trou noir", GLUI_EDITTEXT_INT, NULL, -1, control_cb);
+	information = glui->add_panel("Information" );   //affiche les infos
+	glui-> add_edittext_to_panel(information, "Nb Particule", GLUI_EDITTEXT_INT, NULL, -1, information_cb); 
+	glui-> add_edittext_to_panel(information, "Nb Generateur", GLUI_EDITTEXT_INT, NULL, -1, information_cb);
+	glui-> add_edittext_to_panel(information, "Nb Trou noir", GLUI_EDITTEXT_INT, NULL, -1, information_cb);
 	
-	glui->add_button( "Quit",0,(GLUI_Update_CB)exit );  //doit encore fermer les fenetres et fichiers 
+	glui->add_button( "Quit",0, quit_cb );  //doit encore fermer les fenetres et fichiers puis quitter programme
 	
 	/*quand un widget de GLUI change, un évènement de redisplay est envoyé à la main_gfx_window */
 	glui->set_main_gfx_window( main_window );
@@ -155,16 +171,29 @@ void initOpenGl(void)
 
 }
 
-void file_cb(int control)
+void load_cb(int control)
+{
+	//edittext->get_text();
+}
+
+void save_cb(int control)
 {
 	
 }
 
-void control_cb(int control)
+void simulation_cb(int control)
+{
+	
+}
+void information_cb(int control)
 {
 	
 }
 
+void quit_cb(int control)
+{
+	
+}
 
 void affichage(void)
 {
@@ -196,3 +225,29 @@ void reshape(int w, int h)
 	// création d'un event demandant un ré-affichage
 	glutPostRedisplay();
 }
+
+void mouse(int button, int button_state, int x, int y )
+{
+  if (button == GLUT_LEFT_BUTTON && button_state == GLUT_DOWN ) 
+  {
+    //particule la plus proche selectionnée 
+    //doit rester immobile
+    //peut aussi selectionner generateur ou trou_noir
+  }
+}
+
+void keyboard(unsigned char Key, int x, int y) //si une entité est selectionnée, elle est détruite quand on appuit sur d
+{
+	if(1)  // si une entitée est selectionnée
+	{	switch(Key)
+		{
+			case 'd':
+			break;
+			default :
+			break;
+		}
+	}
+  
+  glutPostRedisplay();
+}
+
